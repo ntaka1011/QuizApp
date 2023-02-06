919fd6451f402aa2038521821f385258f4398eaa
hùng nguyễn viết <nguyenviethung093@gmail.com>
2023-02-06 17:53:44 +0700

    Initial Commit

---
+++ QuizzGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzGameApp
+//
+//  Created by Nguyễn Viết Hùng on 2/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
